df5106b213e410a6eb1f625a5ff0ef9d4bfe495c
Chenxin Yan <yanchenxin2004@gmail.com>
2025-11-09 18:59:12 -0500

    update student schema

--- packages/server/convex/students.ts
+++ packages/server/convex/students.ts
@@ -1,14 +1,32 @@
 import { omit } from "convex-helpers";
 import { protectedMutation, protectedQuery } from "./helpers/auth";
 import { students } from "./schemas/students";
+import { getOneFrom } from "convex-helpers/server/relationships";
 
 export const getCurrentStudent = protectedQuery({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db
+    const student = await ctx.db
       .query("students")
       .withIndex("by_user_id", (q) => q.eq("userId", ctx.user.subject))
       .unique();
+
+    if (student === null) {
+      return null;
+    }
+
+    const school = await getOneFrom(
+      ctx.db,
+      "schools",
+      "by_id",
+      student.school,
+      "_id",
+    );
+
+    return {
+      ...student,
+      school,
+    };
   },
 });
 
