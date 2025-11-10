550e1957153bdf589bbd1744e37c89d853925f7e
Chenxin Yan <yanchenxin2004@gmail.com>
2025-11-09 19:16:14 -0500

    update grade schema

--- apps/web/src/schemas/courses.ts
+++ apps/web/src/schemas/courses.ts
@@ -7,43 +7,23 @@ export const termSchema = z.union([
   z.literal("j-term"),
 ]);
 
-export const gradeSchema = z.union([
-  z.literal("a"),
-  z.literal("a-"),
-  z.literal("b+"),
-  z.literal("b"),
-  z.literal("b-"),
-  z.literal("c+"),
-  z.literal("c"),
-  z.literal("c-"),
-  z.literal("d+"),
-  z.literal("d"),
-  z.literal("p"),
-  z.literal("f"),
-  z.literal("w"),
-]);
-
-export const schoolNameSchema = z.union([
-  z.literal("College of Arts and Science"),
-  z.literal("Graduate School of Arts and Science"),
-  z.literal("College of Dentistry"),
-  z.literal("Gallatin School of Individualized Study"),
-  z.literal("Leonard N. Stern School of Business"),
-  z.literal("Liberal Studies"),
-  z.literal("NYU Abu Dhabi"),
-  z.literal("NYU Shanghai"),
-  z.literal("NYU Grossman School of Medicine"),
-  z.literal("NYU Grossman Long Island School of Medicine"),
-  z.literal("Robert F. Wagner Graduate School of Public Service"),
-  z.literal("Rory Meyers College of Nursing"),
-  z.literal("School of Global Public Health"),
-  z.literal("School of Law"),
-  z.literal("School of Professional Studies"),
-  z.literal("Silver School of Social Work"),
-  z.literal("Steinhardt School of Culture, Education, and Human Development"),
-  z.literal("Tandon School of Engineering"),
-  z.literal("Tisch School of the Arts"),
-]);
+export const gradeSchema = z
+  .union([
+    z.literal("a"),
+    z.literal("a-"),
+    z.literal("b+"),
+    z.literal("b"),
+    z.literal("b-"),
+    z.literal("c+"),
+    z.literal("c"),
+    z.literal("c-"),
+    z.literal("d+"),
+    z.literal("d"),
+    z.literal("p"),
+    z.literal("f"),
+    z.literal("w"),
+  ])
+  .catch("p");
 
 export const userCourseSchema = z.object({
   courseCode: z.string().min(1, "Course code is required"),
