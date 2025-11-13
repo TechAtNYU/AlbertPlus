b0503f5b9e2d15e2fd644646455e2cdab6a06907
Emily Silkina <emily.silkin@gmail.com>
2025-11-13 12:59:27 -0500

    Added form to popup

--- apps/web/src/app/dashboard/account/page.tsx
+++ apps/web/src/app/dashboard/account/page.tsx
@@ -3,19 +3,10 @@
 import { api } from "@albert-plus/server/convex/_generated/api";
 import { useUser } from "@clerk/nextjs";
 import { useConvexAuth, useMutation, useQuery } from "convex/react";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {EditProfilePopup} from "./components/editProfile";
 
 export default function ProfilePage() {
   const { isAuthenticated } = useConvexAuth();
@@ -27,14 +18,6 @@ export default function ProfilePage() {
   );
 
   const upsert = useMutation(api.students.upsertCurrentStudent);
-
-  if (student) {
-    console.log("STUDENTTT");
-    console.log(student.programs);
-  } else{
-     console.log("STUDENT NULL");
-  }
-  
   
 
   return (
@@ -68,39 +51,7 @@ export default function ProfilePage() {
             </p>
         </>
       )}
-
-      <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button variant="outline">Edit Profile</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4">
-            <div className="grid gap-3">
-              <Label htmlFor="name-1">Name</Label>
-              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="username-1">Username</Label>
-              <Input id="username-1" name="username" defaultValue="@peduarte" />
-            </div>
-          </div>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </form>
-    </Dialog>
+      <EditProfilePopup/>
     </div>
   );
 }
