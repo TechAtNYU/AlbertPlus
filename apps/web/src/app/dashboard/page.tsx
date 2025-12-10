85836cf71ae9d11d3a077bc7481b2e6e4688af5d
Michael Wang <146034514+michaelzwang13@users.noreply.github.com>
2025-12-10 18:18:33 -0500

    feat(web): degree progress chart (#54)
    Co-authored-by: Chenxin Yan <yanchenxin2004@gmail.com>

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,8 +1,85 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { api } from "@albert-plus/server/convex/_generated/api";
+import {
+  type RequestForQueries,
+  useConvexAuth,
+  useQueries,
+  useQuery,
+} from "convex/react";
+import type { FunctionReturnType } from "convex/server";
+import { ProgramRequirementsChart } from "@/app/dashboard/components/degree-charts";
 
 const HomePage = () => {
-  // TODO: homepage is not ready yet, hide if from MVP for now
-  redirect("/dashboard/register");
+  const { isAuthenticated } = useConvexAuth();
+  const student = useQuery(
+    api.students.getCurrentStudent,
+    !isAuthenticated ? "skip" : {},
+  );
+
+  const userCourses = useQuery(
+    api.userCourses.getUserCourses,
+    !isAuthenticated ? "skip" : {},
+  );
+
+  const programQueries: RequestForQueries = {};
+
+  if (isAuthenticated && student) {
+    for (const program of student.programs) {
+      programQueries[program._id] = {
+        query: api.programs.getProgramById,
+        args: { id: program._id },
+      };
+    }
+  }
+
+  const programs: Record<
+    string,
+    FunctionReturnType<typeof api.programs.getProgramById>
+  > = useQueries(programQueries);
+
+  // Collect all unique course codes from all programs of type required/alternative
+  const allCourseCodes = new Set<string>();
+  for (const [_, programData] of Object.entries(programs)) {
+    if (programData?.requirements) {
+      for (const requirement of programData.requirements) {
+        if (
+          requirement.type === "required" ||
+          requirement.type === "alternative"
+        ) {
+          for (const courseCode of requirement.courses) {
+            allCourseCodes.add(courseCode);
+          }
+        }
+      }
+    }
+  }
+
+  // Fetch all courses of type required/alternative
+  const courseQueries: RequestForQueries = {};
+  for (const code of allCourseCodes) {
+    courseQueries[code] = {
+      query: api.courses.getCourseByCode,
+      args: { code },
+    };
+  }
+
+  const courses = useQueries(courseQueries);
+
+  const isProgramsLoading =
+    !isAuthenticated ||
+    student === undefined ||
+    Object.keys(programs).length === 0 ||
+    Object.values(programs).some((p) => p === undefined);
+
+  return (
+    <ProgramRequirementsChart
+      programs={programs}
+      userCourses={userCourses}
+      courses={courses}
+      isLoading={isProgramsLoading}
+    />
+  );
 };
 
 export default HomePage;
